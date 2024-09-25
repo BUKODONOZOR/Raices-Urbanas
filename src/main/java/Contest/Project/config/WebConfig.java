b856aefe015d849879aca634a4bdf3acffa44524
BUKODONOZOR <jafetdavidibarguenlozano@pascualbravo.edu.co>
2024-09-24 19:57:24 -0500

    fix13

--- src/main/java/Contest/Project/config/WebConfig.java
+++ src/main/java/Contest/Project/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://raices-urbanas-deploy-9w99.vercel.app")
+                .allowedOrigins("https://raices-urbanas-deploy-4yte.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
