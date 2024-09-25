8852e768ae123b00d7882f01fb6b3b278bb408cf
BUKODONOZOR <jafetdavidibarguenlozano@pascualbravo.edu.co>
2024-09-24 21:40:01 -0500

    fix20

--- src/main/java/Contest/Project/config/SecurityConfig.java
+++ src/main/java/Contest/Project/config/SecurityConfig.java
@@ -1,16 +1,11 @@
 package Contest.Project.config;
 
-import Contest.Project.security.JwtRequestFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
-
-    @Autowired
-    private JwtRequestFilter jwtRequestFilter;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
